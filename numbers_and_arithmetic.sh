1976d468ab2b5939988cad0d77cea06a5fb5c1af
Ma Tko <kwikzzstep@gmail.com>
2021-10-17 20:36:45 +0200

    Hexadecimal operations

--- numbers_and_arithmetic.sh
+++ numbers_and_arithmetic.sh
@@ -28,4 +28,9 @@ echo $(expr $num1 - $num2)
 echo $(expr $num1 % $num2)
 
 
+echo "==========Hexadecimal operations========"
 
+read hex
+
+echo -n "Hexadecimal number for $hex is : "
+echo "obase=10; ibase=16; $hex" | bc
