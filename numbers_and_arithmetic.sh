b2429fcb6fb56064f2dd8f1d3caaaa30e267fe99
Ma Tko <kwikzzstep@gmail.com>
2021-10-17 20:13:33 +0200

    With expressions

--- numbers_and_arithmetic.sh
+++ numbers_and_arithmetic.sh
@@ -4,5 +4,28 @@ num1=15
 
 num2=5
 
+#arithmetic operations
+
 echo $(( num1 + num2 ))
+
+echo $(( num1 / num2 ))
+
+echo $(( num1 * num2 ))
+
+echo $(( num1 - num2 ))
+
+
+echo "=====With expressions======"
+
+echo $(expr $num1 + $num2)
+
+echo $(expr $num1 / $num2)
+
+echo $(expr $num1 * $num2)
+
+echo $(expr $num1 - $num2)
+
+echo $(expr $num1 % $num2)
+
+
 
