ae6e6697cce3c9fd24b1418ce63a7d91d693d76a
Ma Tko <kwikzzstep@gmail.com>
2021-10-17 16:18:54 +0200

    compare strings

---
+++ string_processing.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+str="Hello World!"
+
+#length of the string
+echo ${#str}
+
+#read is a bash built-in command that reads a line from the standard input 
+read str1
+read str2
+
+newstr=$str1$str2
+echo "Concatenated string is '$'newstr"
+
+#compare string
+if [ $str1=$str2 ]
+then 
+    echo "Match"
+
+else
+    echo "No match"
+fi
+
