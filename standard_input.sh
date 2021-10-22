b5c15a5607231725650181c1b2a3645dbd93c8b9
Ma Tko <kwikzzstep@gmail.com>
2021-10-22 17:30:25 +0200

    adder read

--- standard_input.sh
+++ standard_input.sh
@@ -7,3 +7,14 @@ echo $1 $2 $3
 args=("$@")
 
 echo ${args[0]} ${args[1]} 
+echo ${args[0]} ${args[1]} ${args[2]}
+
+echo $#
+
+echo "=========Read var=================="
+echo "Enter first name"
+read fname
+echo "Enter your last name"
+read lname
+
+echo "Hello $fname $lname"
