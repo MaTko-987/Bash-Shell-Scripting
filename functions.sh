405b6af4836b300465d916e5807557f613c74c33
Ma Tko <kwikzzstep@gmail.com>
2021-10-24 09:38:20 +0200

    Function with multiple arguments

--- functions.sh
+++ functions.sh
@@ -2,7 +2,7 @@
 
 #define a function
 
-function my_func1(){
+function my_func1() {
 
     echo "Hello world!"
 
@@ -14,8 +14,21 @@ my_func1
 
 #function with an argument
 
-function my_func2(){
+function my_func2() {
 
     echo $1
 
 }
+
+my_func2 "xyz"
+
+
+#function with multiple arguments
+
+function my_func3() {
+
+    echo "Arguments are : $1 $2 $3 $4"
+
+}
+
+my_func3 "abc" "123" "efg" "456"
