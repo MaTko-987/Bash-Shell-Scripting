6ab16634ba94fb1e07fc36b74e74eb06074603c5
Ma Tko <kwikzzstep@gmail.com>
2021-10-25 11:21:29 +0200

    Print multiple fields

--- introduction_to_AWK.sh
+++ introduction_to_AWK.sh
@@ -16,6 +16,9 @@ then
     
     #Print specific columns in the file
     awk '/testuser/ {print$3}' $fname
+
+    #Print multiple fields
+    awk '/testuser/ {print $2,$3}' $fname
 else
     echo "$fname does not exits"
 
