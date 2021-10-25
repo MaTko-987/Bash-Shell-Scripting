8c1e2be4a38516aa3c5c38a876d2ee61632e4d69
Ma Tko <kwikzzstep@gmail.com>
2021-10-25 07:45:16 +0200

    Check a matching pattern

--- introduction_to_grep.sh
+++ introduction_to_grep.sh
@@ -3,5 +3,17 @@
 #using grep in a script
 
 logfile="testlog.log"
-
 pattern=testserver
+
+
+#check a matching pattern
+
+if grep -q $pattern $logfile
+
+then
+  echo "Match Found."
+
+else 
+  echo "Match Not Found."
+
+fi
