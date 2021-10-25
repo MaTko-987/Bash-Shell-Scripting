818dc6a4cdb42344e9f2410baeeb055fc331a708
Ma Tko <kwikzzstep@gmail.com>
2021-10-25 07:56:56 +0200

    Printinga a line not containing the matched pattern[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D

--- introduction_to_grep.sh
+++ introduction_to_grep.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 #using grep in a script
-
 logfile="testlog.log"
 pattern=testserver
 
 
 #check a matching pattern
-
 if grep -q $pattern $logfile
-
 then
   echo "Match Found."
 
 else 
   echo "Match Not Found."
-
 fi
+
+#printing a line not containing a matched pattern
+grep -v $pattern $logfile
+
+
+
