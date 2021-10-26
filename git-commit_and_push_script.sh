eba9cd6338c241aaf55d656e63bcb0d603bc14d2
Ma Tko <kwikzzstep@gmail.com>
2021-10-27 00:07:28 +0200

    Header grammar fix

--- git-commit_and_push_script.sh
+++ git-commit_and_push_script.sh
@@ -2,7 +2,7 @@
 
 git status
 
-echo '==============Add file/s: ==============='
+echo '============== Add file/s: ==============='
 read file
 
 #if [ file=="."] || [ file==""]
@@ -10,11 +10,11 @@ read file
 
 git add $file
 
-echo '==============Commit/s: ================='
+echo '============== Commit: ================='
 read commit_Message
 git commit -m "$commit_Message"
 
-echo '==============Branch/es : ================='
+echo '============== Branch: ================='
 read branch
 git push -f origin $branch
 
