c228d70b4b145564d960ca945566858059309a1f
Ma Tko <kwikzzstep@gmail.com>
2021-10-27 00:05:20 +0200

    Added plural to headings

--- git-commit_and_push_script.sh
+++ git-commit_and_push_script.sh
@@ -2,7 +2,7 @@
 
 git status
 
-echo '==============Add file: ==============='
+echo '==============Add file/s: ==============='
 read file
 
 #if [ file=="."] || [ file==""]
@@ -10,11 +10,11 @@ read file
 
 git add $file
 
-echo '==============Commit: ================='
+echo '==============Commit/s: ================='
 read commit_Message
 git commit -m "$commit_Message"
 
-echo '==============Branch: ================='
+echo '==============Branch/es : ================='
 read branch
 git push -f origin $branch
 
